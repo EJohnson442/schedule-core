ee3cabf1d7b8ead110f4ed44700baab0a0e4b7a2
ejohnon442 <ejohnson442@gmail.com>
2019-01-20 15:38:22 -0500

    worker_helper.rb replaces init_worker.rb

---
+++ helpers/worker_helper.rb
@@ -0,0 +1,30 @@
+require 'consecutive_days'
+require_relative 'files_helper'
+require 'config'
+require 'logging'
+
+module Worker_helper
+    class Worker_data_classes
+        include Files_helper
+        attr_reader :data
+
+        def initialize(daily_task_list)
+            @data = []
+            daily_task_list.uniq.each { |p| @data << create_worker_classes(p) }
+        end
+
+        def create_worker_classes(task)
+            worker_class = Config::worker_registry(task)
+            worker_class.new(task) {|f| load_data(f)}
+        end
+
+        def load_data(task)
+            load_file_data(Config::config_data['data_dir'] + task.to_s.capitalize + ".dat")
+        end
+
+        def load_file_data(full_filename)
+            data = read_file_n2_array(full_filename)
+            data.shuffle!
+        end
+    end
+end
