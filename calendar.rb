0bf4a250ba03e78d7ce7ef2b4aab7f693fc4cf66
EJohnson442 <EJohnson442@gmail.com>
2021-07-28 13:14:51 -0400

    Added 'calendar.rb', 'load_data_files.rb'. Both are just renamed files

---
+++ calendar.rb
@@ -0,0 +1,83 @@
+require 'date'
+require 'json'
+
+module Calendar_formats
+    FORMATS = [:native, :json, :task]
+    CALENDAR_DATA = Struct.new(:fmt, :calendar, :daily_task_list, :schedule)
+    
+    Native_calendar = Struct.new(:calendar, :daily_task_list, :workers) do
+        def generate_calendar()
+            schedule = []
+            worker_list = []
+            workers.each do |a|
+                position = a.keys[0].id2name
+                worker_list << position + " = " + a.values[0]
+            end
+
+            (0..calendar.length - 1).each do
+                daily_workers = worker_list.shift(daily_task_list.length)
+                daily_workers.insert(0,calendar.shift)
+                schedule << daily_workers
+            end
+            schedule
+        end
+    end
+    
+    JSON_calendar = Struct.new(:calendar, :schedule) do
+        def generate_calendar()
+            hash_schedule = {}
+            daily_assignments = schedule.length / calendar.length
+            calendar.each{|day| hash_schedule[day] = hash_roster(schedule.shift(daily_assignments))}     #problem is here
+            JSON.generate(hash_schedule)
+        end
+        
+        def hash_roster(roster)
+            roster_data = {}
+            roster.each do |data|
+                if roster_data.has_key?data.keys[0]
+                    roster_data[data.keys[0]] = manage_array(roster_data[data.keys[0]], data.values[0])
+                else
+                    roster_data[data.keys[0]] = data.values[0]
+                end
+            end
+            roster_data
+        end
+        
+        def manage_array(curr_value,new_value)
+            if curr_value.class == Array
+                curr_value << new_value
+            else
+                arr_value = []
+                arr_value << curr_value
+                arr_value << new_value
+                curr_value = arr_value
+            end
+        end
+    end
+
+    Task_view_calendar = Struct.new(:calendar) do
+        def generate_calendar()
+            schedule = []
+            calendar.each do |schedule_day|
+                schedule_day.each {|worker| schedule << "#{worker}"}
+                schedule << " "
+            end
+        end
+    end
+
+    def calendar(calendar_data)
+        calendar_run = nil
+        case calendar_data.fmt
+            when :native
+                calendar_run = Native_calendar.new(calendar_data.calendar, calendar_data.daily_task_list, calendar_data.schedule)
+            when :json
+                calendar_run = JSON_calendar.new(calendar_data.calendar, calendar_data.schedule)
+            when :task
+                native_run = Native_calendar.new(calendar_data.calendar, calendar_data.daily_task_list, calendar_data.schedule)
+                calendar_run = Task_view_calendar.new(native_run.generate_calendar())
+            else    
+                raise RuntimeError.new("'generate_calendar' must be one of the following: #{FORMATS}")
+        end
+        calendar_run.generate_calendar()
+    end
+end
---
+++ load_data_files.rb
@@ -0,0 +1,31 @@
+require 'consecutive_days'
+require_relative 'config'
+require_relative 'logging'
+
+module Prep_schedule
+    include Config
+
+    class Schedule_classes
+        attr_reader :data
+
+        def initialize(schedule_type_list)
+            @data = []
+            schedule_type_list.uniq.each { |p| @data << create_schedule_classes(p) }
+        end
+
+        def create_schedule_classes(schedule_type)
+            schedule_class = Config::get_worker_classes(schedule_type)
+            schedule_class.new(schedule_type) {|f| load_workers(Config.config_data['data_dir'] + schedule_type.to_s.capitalize + ".dat")}
+        end
+
+        def load_workers(full_filename)
+            data = []
+            File.open(full_filename, "r") do |f|
+                f.each_line do |line|
+                    line.include?("\n") ? data << line.chop! : data << line
+                end
+            end
+            data.shuffle!
+        end
+    end
+end
