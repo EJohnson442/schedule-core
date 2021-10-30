a2a52e0c8c5f8df6b6eff319d0893adfd7169ef1
Ernest Johnson <ejohnson442@gmail.com>
2021-10-29 21:33:04 -0400

    work/worker_helper:  Refactored validation

--- work/worker_helper.rb
+++ work/worker_helper.rb
@@ -18,34 +18,20 @@ module Validate
     end
 
     def candidate_in_prior_weeks?(worker_data)
-        data_range_config = {}
-        data_range_config[:data_length] = worker_data.data.length
-        data_range_config[:daily_tasks_count] = worker_data.daily_tasks_list_count
-        data_range_config[:schedule_count] = worker_data.scheduled_days_count
-        #weeks is usually >= 1. Determines how many weeks to consider including partial week
-        #weeks = 0 will set :partial_week to true and: 
-        #1) Calculate only remaining partial week. Check only the current week for the occurance of a worker
-        #2) Review only prior weeks
-		if worker_data.weeks == 0
-        	data_range_config[:partial_week] = true
-		else
-        	data_range_config[:partial_week] = false
-		end
-        data_range_config[:weeks] = worker_data.weeks
-        data_range_array = get_data_range_array(data_range_config)
+        data_range_array = get_data_range_array(worker_data)
         found_candidate?(worker_data.candidate, worker_data.data, data_range_array)
     end
 
-    def get_data_range_array(config)
-        one_week = config[:daily_tasks_count] * config[:schedule_count]
-        part_week = config[:data_length] % one_week
-        if config[:partial_week]
-            start_ndx = config[:data_length] < one_week ? 0 : config[:data_length] - (part_week)
+    def get_data_range_array(worker)
+        one_week = worker.daily_tasks_list_count * worker.scheduled_days_count
+        part_week = worker.data.length % one_week
+        if worker.weeks == 0   #partial week
+            start_ndx = worker.data.length < one_week ? 0 : worker.data.length - (part_week)
         else
-            start_ndx = config[:data_length] - ((one_week * config[:weeks]) + part_week)
+            start_ndx = worker.data.length - ((one_week * worker.weeks) + part_week)
         end
         start_ndx = 0 if start_ndx < 0
-        end_ndx = config[:data_length] - 1
+        end_ndx = worker.data.length - 1
                     
         [start_ndx, end_ndx]
     end	
